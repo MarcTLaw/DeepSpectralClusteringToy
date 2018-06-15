4fa1931f902f6d31fa9485a6d37b135e236d3553
MarcTLaw <marclaw@hotmail.com>
2018-06-14 21:19:54 -0400

    Update plot_test_data.m

--- plot_test_data.m
+++ plot_test_data.m
@@ -12,7 +12,7 @@ end
 
 if use_singular_vectors
     [X, ~, ~] = svd(X, 'econ');
-    X = X ./ repmat(sum(X.^2,2),1,size(X,2));
+    X = X ./ repmat(sqrt(sum(X.^2,2)),1,size(X,2));
 end
 
 labels = load('saved_data/test_labels.txt');
@@ -20,3 +20,5 @@ X1 = X(labels == 1,:);
 X2 = X(labels == 2,:);
 X3 = X(labels == 3,:);
 plot3(X1(:,1), X1(:,2), X1(:,3), 'bx', X2(:,1), X2(:,2), X2(:,3), 'ro', X3(:,1), X3(:,2), X3(:,3), 'gs')
+
+axis equal
